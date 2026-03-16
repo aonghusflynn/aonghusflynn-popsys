f625b4f638f1569fc2b6e6d12fc8b87687768f60
Aonghus Flynn <developer@aonghusflynn.com>
2026-03-16 21:50:27 +0000

    feat: register photo_project custom post type

--- aonghus-photography/inc/post-types.php
+++ aonghus-photography/inc/post-types.php
@@ -1,1 +1,30 @@
 <?php
+/**
+ * Registers the photo_project custom post type.
+ */
+function aonghus_register_photo_project_cpt() {
+    $labels = [
+        'name'               => 'Projects',
+        'singular_name'      => 'Project',
+        'add_new_item'       => 'Add New Project',
+        'edit_item'          => 'Edit Project',
+        'new_item'           => 'New Project',
+        'view_item'          => 'View Project',
+        'search_items'       => 'Search Projects',
+        'not_found'          => 'No projects found.',
+        'not_found_in_trash' => 'No projects found in trash.',
+    ];
+
+    $args = [
+        'labels'              => $labels,
+        'public'              => true,
+        'has_archive'         => false,
+        'rewrite'             => [ 'slug' => 'projects' ],
+        'supports'            => [ 'title', 'editor', 'thumbnail', 'page-attributes' ],
+        'menu_icon'           => 'dashicons-camera',
+        'show_in_rest'        => false,
+    ];
+
+    register_post_type( 'photo_project', $args );
+}
+add_action( 'init', 'aonghus_register_photo_project_cpt' );
